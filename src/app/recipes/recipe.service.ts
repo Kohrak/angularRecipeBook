c56e083975e54ec9b0821610d88d4a46169ef6b0
Gabriel@Electrika <gabriel@electrika.com>
2018-08-03 12:36:43 +0100

    show ingredients on recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,13 @@ export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('First recipe', 'this is a test', 'https://www.curiouscuisiniere.com/wp-content/uploads/2017/02/Venezuelan-Arepas-1054.21.jpg')
+    new Recipe('First recipe', 
+    'this is a test', 
+    'https://www.curiouscuisiniere.com/wp-content/uploads/2017/02/Venezuelan-Arepas-1054.21.jpg',
+  [
+    new Ingredient('Harina Pan', 1), 
+    new Ingredient('salt', 1)
+  ])
   ];
   constructor() { }
 
