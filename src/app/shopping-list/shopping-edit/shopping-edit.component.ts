0569d08d9e0278c94e6f782eb5fba449ecff841d
Gabriel@Electrika <gabriel@electrika.com>
2018-08-03 08:41:14 +0100

    allow user to add ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output() shoppingAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAdd(nameField: HTMLInputElement, amountField: HTMLInputElement){
+      let name = nameField.value
+      let amount = amountField.value
+      let newIngredient = new Ingredient(name, Number(amount))
+      nameField.value = "";
+      amountField.value = "";
+      this.shoppingAdded.emit(newIngredient)
   }
 
 }
