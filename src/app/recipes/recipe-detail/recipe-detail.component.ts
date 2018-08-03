ff8202258f40de29eb6a0132a5fc53e19fe2707d
Gabriel@Electrika <gabriel@electrika.com>
2018-08-03 12:43:23 +0100

    sned ingredients to the shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingService } from '../../shopping-list/shopping.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +11,15 @@ export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
   
-  constructor() { }
+  constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit() {
+  }
+
+  onSendToShoppingList(){
+    for (let element of this.recipe.ingredients) {
+      this.shoppingService.onAdd(element.name, element.amount)
+    }
   }
 
 }
